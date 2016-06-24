faede15fc7b21887f3aa1da36c105b9e5fed5566
Yen-Hsun, Huang <s011208@gmail.com>
2016-06-24 13:00:26 +0800

    add about

--- app/src/main/java/yhh/bj4/lotterylover/settings/main/MainSettingsFragment.java
+++ app/src/main/java/yhh/bj4/lotterylover/settings/main/MainSettingsFragment.java
@@ -1,11 +1,15 @@
 package yhh.bj4.lotterylover.settings.main;
 
 import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceScreen;
+import android.widget.Toast;
 
 import yhh.bj4.lotterylover.LotteryLover;
 import yhh.bj4.lotterylover.R;
@@ -20,6 +24,8 @@ public class MainSettingsFragment extends PreferenceFragment {
 
     private static final String SETTINGS_DISPLAY_ORDER = "settings_display_order";
     private static final String SETTINGS_DISPLAY_ROW_COUNT = "settings_display_row_count";
+    private static final String SETTINGS_ABOUT_APP_VERSION = "settings_about_app_version";
+    private static final String SETTINGS_ABOUT_CONTACT_ME = "settings_about_contact_me";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -41,6 +47,14 @@ public class MainSettingsFragment extends PreferenceFragment {
         pref = findPreference(SETTINGS_DISPLAY_ROW_COUNT);
         if (pref != null) {
             pref.setSummary(getActivity().getResources().getStringArray(R.array.settings_display_rows)[AppSettings.get(getActivity(), LotteryLover.KEY_DISPLAY_ROWS, LotteryLover.DISPLAY_ROWS_100)]);
+        }
+
+        pref = findPreference(SETTINGS_ABOUT_APP_VERSION);
+        if (pref != null) {
+            try {
+                pref.setSummary(getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0).versionName);
+            } catch (PackageManager.NameNotFoundException e) {
+            }
         }
     }
 
@@ -119,6 +133,21 @@ public class MainSettingsFragment extends PreferenceFragment {
                         }
                     }).create();
             dialog.show();
+        } else if (SETTINGS_ABOUT_CONTACT_ME.equals(key)) {
+            try {
+                String emailAddress = "s011208@gmail.com";
+                String emailSubject = "Hi, I'd like to contact with you";
+                String emailMessage = "";
+                Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{emailAddress});
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, emailSubject);
+                emailIntent.setType("plain/text");
+                emailIntent.putExtra(Intent.EXTRA_TEXT, emailMessage);
+                startActivity(emailIntent);
+                return true;
+            } catch (ActivityNotFoundException e) {
+                Toast.makeText(getActivity(), "unexpected error", Toast.LENGTH_LONG).show();
+            }
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
