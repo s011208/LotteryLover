28c52b2b868d74069ab5961995e1c76f2c7d95aa
Yen-Hsun, Huang <s011208@gmail.com>
2016-06-27 17:53:54 +0800

    add lib DragSortListView

--- app/src/main/java/yhh/bj4/lotterylover/settings/ltotype/LtoTypeSettingFragment.java
+++ app/src/main/java/yhh/bj4/lotterylover/settings/ltotype/LtoTypeSettingFragment.java
@@ -1,9 +1,28 @@
 package yhh.bj4.lotterylover.settings.ltotype;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mobeta.android.dslv.DragSortListView;
 
 /**
  * Created by yenhsunhuang on 2016/6/27.
  */
 public class LtoTypeSettingFragment extends Fragment {
+    private DragSortListView mLtoTypeList;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
